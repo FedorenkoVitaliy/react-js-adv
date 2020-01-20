3059e51c45a473fbb70fa3ef53da8f2ae44d96fb
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-20 18:27:33 +0200

    learn(loading): add loading component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import Loader from "./Loader";
 
 import './App.css';
 import AddTodo from "./Todo/AddTodo";
 
 function App() {
     const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
             .then(response => response.json())
             .then(todos => {
                 setTimeout(() => {
-                    setTodos(todos)
-                }, 2000)
+                    setTodos(todos);
+                    setLoading(false);
+                }, 2000);
+
+            })
+            .catch(e => {
+                console.log('Ошибочка');
+                //setLoading(true);
             })
     }, []);
 
@@ -49,14 +57,18 @@ function App() {
                 <h1>React Tutorial</h1>
 
                 <AddTodo onCreate={addTodo}/>
-                {
-                    todos.length ?
+                { loading && <Loader/> }
+                { todos.length ? (
                     <TodoList todos={todos}
                               onToggle={toggleTodo}
-                    /> :
-                    <p>No Todos</p>
-                }
+                    />
+                ) : (
+                    loading?
+                        null:
+                        <p>No Todos</p>
+                )
 
+                }
             </div>
         </Context.Provider>
     );
