fd132eae29aef0310de06474da77e8c21d789714
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-20 17:45:06 +0200

    learn(useEffect): get data from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 
@@ -6,13 +6,17 @@ import './App.css';
 import AddTodo from "./Todo/AddTodo";
 
 function App() {
-    let initialTodos = [
-        {id: 1, completed: false, title: 'Купить хлеб'},
-        {id: 2, completed: true, title: 'Купить масло'},
-        {id: 3, completed: false, title: 'Купить молоко'},
-    ];
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, setTodos] = useState([]);
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                }, 2000)
+            })
+    }, []);
 
     function toggleTodo(id) {
         setTodos(
