8b648b1e3e13a3cc6946f605aa2f938ec775a5b9
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-20 16:21:10 +0200

    feat(App): structure and styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import TodoList from "./Todo/TodoList";
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+    const todos = [
+        {id: 1, completed: false, title: 'Купить хлеб'},
+        {id: 2, completed: false, title: 'Купить масло'},
+        {id: 3, completed: false, title: 'Купить молоко'},
+    ];
+    return (
+        <div className="wrapper">
+            <h1>React Tutorial</h1>
+
+            <TodoList todos={todos}/>
+        </div>
+    );
 }
 
 export default App;
